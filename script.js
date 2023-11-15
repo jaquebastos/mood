266c8896ed0e5e4a1191a28e05ccc5f3d1ba9795
jaquenbastos <linabw.bastos@gmail.com>
2023-11-15 13:43:10 -0300

    mood

---
+++ script.js
@@ -0,0 +1,28 @@
+var imagem = document.getElementById("imagem4")
+var imagem = document.getElementById("imagem5")
+
+// iih
+function bt1() {
+  return imagem1.innerHTML = "<img src='https://i.pinimg.com/236x/7b/c8/7b/7bc87b5554ae05b6d679678ebd13e376.jpg' />"
+}
+
+//tímido
+function bt2() {
+  return imagem2.innerHTML = "<img src='https://i.pinimg.com/236x/b7/b7/fa/b7b7fae4972046ef178dce01650ca7fc.jpg' />"
+}
+
+//pensativo
+function bt3() {
+  return imagem3.innerHTML = "<img src='https://i.pinimg.com/236x/14/15/70/1415704b87823802cfacca8022992ab9.jpg' />"
+}
+
+//que tistreza
+function bt4() {
+  return imagem4.innerHTML = "<img src='https://i.pinimg.com/236x/77/3c/37/773c378a6d35cffa0e23f856b9bd9c0a.jpg' />"
+}
+
+
+//bravo
+function bt5() {
+  return imagem5.innerHTML = "<img src='https://i.pinimg.com/236x/92/48/b7/9248b770ac5c48b619c217fa38174791.jpg' />"
+}
